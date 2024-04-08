16bcaf51908c2c48e437f6eef8dc9e379ceee84f
Ivan H Carvajal A <ivanchoh99@gmail.com>
2024-04-07 19:47:15 -0500

    Dockerizacion y configuracion

--- mcsv-citas/src/main/java/com/citas/client/UsuarioClientREST.java
+++ mcsv-citas/src/main/java/com/citas/client/UsuarioClientREST.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "usuarios-mcsv", url = "localhost:8002")
+@FeignClient(name = "usuarios-mcsv", url = "usuarios-mcsv:8002")
 public interface UsuarioClientREST {
 
     @GetMapping("/{id}")
